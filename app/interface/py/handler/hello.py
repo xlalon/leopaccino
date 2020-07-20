e481e9495022e52b57c85fb4cf1fb44b5b18d56f
Leo <xiaoli@qiushibaike.com>
2020-07-20 16:57:50 +0800

    add pymysql

--- app/interface/py/handler/hello.py
+++ app/interface/py/handler/hello.py
@@ -1,18 +1,31 @@
 # -*- coding:utf8 -*-
 
 from redis import Redis
+from pymysql import connect
+from pymysql.cursors import DictCursor
 
 from app.infra.celery.task.test import add
 
 from .base import BaseHandler
 
-redis = Redis(host='redis', port=6379)
+redis = Redis(host='leopaccino_redis', port=6379)
+connection = connect(host='leopaccino_mysql',
+                     user='leo',
+                     password='Xiao0000',
+                     database='leo_web',
+                     charset='utf8mb4',
+                     cursorclass=DictCursor)
 
 
 class HelloHandler(BaseHandler):
     def get(self):
         count = redis.incr('hits')
-        self.write('Hello World! 该页面已被访问 {} 次。\n'.format(count))
+        with connection.cursor() as cursor:
+            # Create a new record
+            sql = "SELECT * FROM test"
+            cursor.execute(sql)
+            data = cursor.fetchall()
+        self.write('Hello World!\n 该页面已被访问 {} 次。\n test values:{}\n'.format(count, data))
 
 
 class TestHandler(BaseHandler):
